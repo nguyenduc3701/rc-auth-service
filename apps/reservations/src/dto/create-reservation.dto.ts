74a6a6ac0bf4a52f65ee241234b46acb966269d2
minh duc nguyen <duc.nguyenminh@ascendcorp.com>
2025-05-27 18:08:36 +0700

    first run with 'docker-compose up'

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -3,11 +3,9 @@ import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateReservationDto {
   @IsDate()
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @Type(() => Date)
   startDate: Date;
   @IsDate()
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   @Type(() => Date)
   endDate: Date;
   @IsString()
